726d887698ec5d2d2c46666f8b234af0a3aea309
robinson1919 <46830749+robinson1919@users.noreply.github.com>
2020-04-10 04:31:23 -0400

    First Commit

---
+++ NuevoCrud/Controllers/CrudController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NuevoCrud.Controllers
+{
+    public class CrudController : Controller
+    {
+        // GET: Crud
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult listar()
+        {
+            nuevoEntities db = new nuevoEntities();
+            var listar = db.cruds.Where(p => p.active.Equals(1))
+                .Select(p => new { p.id, p.firstName, p.lastName, p.age }).ToList();
+
+            return Json(listar, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult recuperarInfo(int id)
+        {
+            nuevoEntities db = new nuevoEntities();
+            var lista = db.cruds.Where(p=> p.active.Equals(1) && p.id.Equals(id))
+                .Select(p => new { p.id, p.firstName, p.lastName, p.age }).ToList();
+
+            return Json(lista, JsonRequestBehavior.AllowGet);
+        }
+
+        public int save(crud iForm)
+        {
+            nuevoEntities db = new nuevoEntities();
+            var nRegistrosAfectados = 0;
+            try
+            {
+                if (iForm.id == 0)
+                {
+                    //crud iForm = new crud();
+                    db.cruds.Add(iForm);
+                    db.SaveChanges();
+                    nRegistrosAfectados = 1;
+                }
+                else
+                {
+                    crud iData = db.cruds.Where(p => p.id.Equals(iForm.id)).First();
+                    iData.firstName = iForm.firstName;
+                    iData.lastName = iForm.lastName;
+                    iData.age = iForm.age;
+                    iData.active = iForm.active;
+                    db.SaveChanges();
+                    nRegistrosAfectados = 1;
+                }
+            }
+            catch(Exception ex)
+            {
+                nRegistrosAfectados = 0;
+            }
+            return nRegistrosAfectados;
+        }
+
+        public int eliminar(int id)
+        {
+            nuevoEntities db = new nuevoEntities();
+            var nRegistrosAfectados = 0;
+            try
+            {
+                
+                crud iData = db.cruds.Where(p => p.id.Equals(id)).First();
+                iData.active = 0;
+                db.SaveChanges();
+                nRegistrosAfectados = 1;
+            }
+            catch(Exception ex)
+            {
+                nRegistrosAfectados = 0;
+            }
+            return nRegistrosAfectados;
+        }
+    }
+}
---
+++ NuevoCrud/Scripts/crud.js
@@ -0,0 +1,106 @@
+﻿
+listar();
+function listar() {
+    $.get("Crud/listar", function (data) {
+        //console.log(data);
+        var arrayColumns = [];
+        arrayColumns = Object.keys(data[0]);
+        crearListado(arrayColumns, data);
+    })
+}
+
+function crearListado(arrayColumns, data){
+    var contenido = '';
+    contenido += '<table class="table">';
+    contenido += '<thead>';
+    contenido += '<th scope="col">ID</th>';
+    contenido += '<th scope="col">Nombre</th>';
+    contenido += '<th scope="col">Apellido</th>';
+    contenido += '<th scope="col">Edad</th>';
+    contenido += '<th scope="col">Aciones</th>';
+    contenido += '</tr>';
+    contenido += '</thead>';
+    contenido += '<tbody>';
+    var nData = data.length;
+    var nArrayColumns = arrayColumns.length;
+    for (var i = 0; i < nData; i++) {
+        contenido += '<tr>';
+        for (var j = 0; j < nArrayColumns; j++) {
+            var countArrayColumns = arrayColumns[j];
+            contenido += '<td>' + data[i][countArrayColumns] + '</td>';
+            
+        }
+        var id = data[i].id;
+        contenido += '<td><button class="btn btn-warning" onClick="openModal(' + id +')" data-toggle="modal" data-target="#exampleModal">Editar</button>&nbsp&nbsp';
+        contenido += '<button class="btn btn-danger" onClick="eliminar(' + id +')">Eliminar</button></td>';
+    }
+    
+    contenido += '</tr>';
+    contenido += '</tbody>';
+    contenido += '</table>';
+
+    document.querySelector('#table').innerHTML = contenido;
+
+
+}
+
+function openModal(id) {
+    if (id === 0) {
+        borrarInputs();
+    } else {
+        $.get("Crud/recuperarInfo/?id=" + id, function (data) {
+            console.log(data);
+            var id = data[0].id;
+            var nombre = data[0].firstName;
+            var apellido = data[0].lastName;
+            var edad = data[0].age;
+
+            document.querySelector('#id').value = id;
+            document.querySelector('#nombre').value = nombre;
+            document.querySelector('#apellido').value = apellido;
+            document.querySelector('#edad').value = edad;
+
+        });
+    }
+}
+
+function borrarInputs() {
+    var inputs = document.querySelectorAll('.form-control');
+    var nInputs = inputs.length;
+    for (var i = 0; i < nInputs; i++) {
+        inputs[i].value = '';
+    }
+}
+
+function agregar() {
+    var datos = new FormData();
+    var id = document.querySelector('#id').value;
+    var nombre = document.querySelector('#nombre').value;
+    var apellido = document.querySelector('#apellido').value;
+    var edad = document.querySelector('#edad').value;
+
+    datos.append('id', id);
+    datos.append('firstName', nombre);
+    datos.append('lastName', apellido);
+    datos.append('age', edad);
+    datos.append('active', 1);
+
+    $.ajax({
+        type: 'POST', 
+        url: 'Crud/save',
+        data: datos,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            console.log(response);
+            listar();
+            document.querySelector('.close').click();
+        }
+    })
+}
+
+function eliminar(id) {
+    $.get("Crud/eliminar/?id=" + id, function (data) {
+        listar();
+    })
+}
